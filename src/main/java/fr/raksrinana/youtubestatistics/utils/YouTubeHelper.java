134c0ac6243c01685cdb6058dafd066ec070b282
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-01-15 10:24:27 +0100

    Fix update version

--- src/main/java/fr/raksrinana/youtubestatistics/utils/YouTubeHelper.java
+++ src/main/java/fr/raksrinana/youtubestatistics/utils/YouTubeHelper.java
@@ -8,7 +8,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.youtube.YouTube;
 import fr.raksrinana.youtubestatistics.Main;
 import fr.raksrinana.youtubestatistics.interfaces.IYouTubeVideo;
@@ -33,7 +33,7 @@ public class YouTubeHelper{
 	@Getter
 	private static final String APPLICATION_NAME = "YouTubeStatistics";
 	@Getter
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 	private final YouTube youtube;
 	private static YouTube SERVICE;
 	@Getter
